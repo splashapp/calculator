4a59d467049d12279ba7dcccbcbe85c0981cdcde
OLIVER RUESS <external.oliver.ruess@kuehne-nagel.com>
2018-01-21 15:16:49 +0100

    Add Wallaby and implemet tests

--- src/App.js
+++ src/App.js
@@ -3,16 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+  onInputChange(e) {
+    const val = e.target.value // ?
+    const parts = val.match(/(\d*)([\+*])(\d+)/) // ?
+    parts
+    if (!parts) return
+    const left = parseInt(parts[1]) // ?
+    const operator = parts[2] // ?
+    const right = parseInt(parts[3]) // ?
+    let result
+    if(operator === '+') {
+      result = left + right //?
+    } else if (operator === '*') {
+      result = left * right //?
+    }
+    this.setState({result})
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={this.onInputChange.bind(this)}/>
+        <div id="result">{this.state.result}</div>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({adapter: new Adapter()})
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it.only('Avoid crash on partial input', () => {
+  const wrapper = shallow(<App />)
+  wrapper.find('input').simulate('change', {
+    target: {value: '24'}
+  })
+  const renderedResult = wrapper.find('#result').text()
+  expect(renderedResult).toBe('')
+})
+
+it('multiply', () => {
+  const wrapper = shallow(<App />)
+  wrapper.find('input').simulate('change', {
+    target: {value: '24*2'}
+  })
+  const renderedResult = wrapper.find('#result').text()
+  expect(renderedResult).toBe('48')
+})
+
+it('add', () => {
+  const wrapper = shallow(<App />)
+  wrapper.find('input').simulate('change', {
+    target: {value: '25+10'}
+  })
+  const renderedResult = wrapper.find('#result').text()
+  expect(renderedResult).toBe('35')
+})
